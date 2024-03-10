2e6f59e8c3fbeda9cc1b3de79463ad0afc549d78
zenginhayri3 <zenginhayri3@gmail.com>
2024-03-10 17:49:03 +0300

    Add project files.

---
+++ Business/Concrete/InstructorManager.cs
@@ -0,0 +1,48 @@
+﻿using Business.Abstract;
+using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Concrete
+{
+    public class InstructorManager : IInstructorService
+    {
+        IInstructorDal _ınstructorDal;
+
+        public InstructorManager(IInstructorDal ınstructorDal)
+        {
+            _ınstructorDal = ınstructorDal;
+        }
+
+        public void Add(Instructor ınstructor)
+        {
+            _ınstructorDal.Add(ınstructor);
+        }
+
+        public void Delete(Instructor ınstructor)
+        {
+            _ınstructorDal.Delete(ınstructor);
+
+        }
+
+        public List<Instructor> GetAll()
+        {
+            return _ınstructorDal.GetAll();
+        }
+
+        public Instructor GetById(int id)
+        {
+            return _ınstructorDal.Get(ı=>ı.InstructorId==id);
+        }
+
+        public void Update(Instructor ınstructor)
+        {
+            _ınstructorDal.Update(ınstructor);
+
+        }
+    }
+}
---
+++ Entities/Concrete/Course.cs
@@ -0,0 +1,20 @@
+﻿using Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.Concrete
+{
+    public class Course:IEntity
+    {
+        public int Id { get; set; }
+        public int CategoryId { get; set; }
+        public int InstructorId { get; set; }
+        public string CourseImage { get; set; }
+        public string Description { get; set; }
+        public int CompletionRate { get; set; }
+
+    }
+}
